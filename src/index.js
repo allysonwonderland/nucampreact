4f3c70640280ae5f6056612cc95410de3b02759f
Ally <allyson.lifter@gmail.com>
2020-09-20 18:14:19 -0400

    React Week 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+
+
 
 
 ReactDOM.render(
